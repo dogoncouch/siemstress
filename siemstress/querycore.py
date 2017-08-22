5fa807cc2ba490fbdfe08ab89e37c55001a21410
dogoncouch <dpersonsdev@gmail.com>
2017-08-22 16:21:51 -0400

    Debug dict query

--- siemstress/querycore.py
+++ siemstress/querycore.py
@@ -245,13 +245,13 @@ class QueryCore:
 
         if self.args.verbose: print("SQL:\n" + qstatement)
 
-        print("%7s %20s %18s %14s %12s %s" % (
-                desc[self.queryfields[0]][0],
-                desc[self.queryfields[1]][0],
-                desc[self.queryfields[2]][0],
-                desc[self.queryfields[3]][0],
-                desc[self.queryfields[4]][0],
-                desc[self.queryfields[5]][0]))
+        #print("%7s %20s %18s %14s %12s %s" % (
+        #        desc[self.queryfields[0]][0],
+        #        desc[self.queryfields[1]][0],
+        #        desc[self.queryfields[2]][0],
+        #        desc[self.queryfields[3]][0],
+        #        desc[self.queryfields[4]][0],
+        #        desc[self.queryfields[5]][0]))
 
         for row in rows:
             print("%7s %20s %18s %14s %12s %s" % (
