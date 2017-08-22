7d896fb4b2d98ba0aeb0f92201a4dd005b7cac9f
dogoncouch <dpersonsdev@gmail.com>
2017-08-21 21:20:29 -0400

    Debug query

--- siemstress/query.py
+++ siemstress/query.py
@@ -74,7 +74,7 @@ class SiemQuery:
 
         return desc, rows
 
-    def query(tables=[], columns=[],
+    def query(self, tables=[], columns=[],
             last=None, daterange=None, sourcehosts=[], sourceports=[],
             desthosts=[], destports=[], processes=[],
             pids=[], protocols=[], greps = [],
