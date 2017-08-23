7febb246105bf524b6a25a17704cd0afe1227c11
dogoncouch <dpersonsdev@gmail.com>
2017-08-23 06:20:14 -0400

    Add packages to __init__.py

--- siemstress/__init__.py
+++ siemstress/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.2'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/siemstress'
-__all__ = ['core', 'querycore', 'query']
+__all__ = ['parsecore', 'querycore', 'query', 'triggercore', 'trigger']
 
 import siemstress.query
 import siemstress.trigger
