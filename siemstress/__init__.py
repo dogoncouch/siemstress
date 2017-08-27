9ec465771ed3b6b1b0be85468f73086fcfdbb76d
dogoncouch <dpersonsdev@gmail.com>
2017-08-27 14:56:54 -0400

    Add util module for DB testing

--- siemstress/__init__.py
+++ siemstress/__init__.py
@@ -2,7 +2,9 @@ __version__ = '0.4-alpha'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/siemstress'
-__all__ = ['parsecore', 'querycore', 'query', 'triggercore', 'trigger']
+__all__ = ['parsecore', 'querycore', 'query', 'triggercore', 'trigger',
+        'util']
 
 import siemstress.query
 import siemstress.trigger
+import siemstress.util
