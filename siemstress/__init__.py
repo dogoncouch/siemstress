7d8d6304fb7d9399f0b7eb60bd634d270cceb214
dogoncouch <dpersonsdev@gmail.com>
2017-09-01 23:57:10 -0400

    Update package auto-imports

--- siemstress/__init__.py
+++ siemstress/__init__.py
@@ -2,9 +2,11 @@ __version__ = '0.6-alpha'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/siemstress'
-__all__ = ['parsecore', 'querycore', 'query', 'triggercore', 'trigger',
-        'util']
+__all__ = ['parsecore', 'parse', 'querycore', 'query', 'triggercore',
+        'trigger', 'managecore', 'manage', 'util']
 
+import siemstress.parse
 import siemstress.query
 import siemstress.trigger
+import siemstress.manage
 import siemstress.util
