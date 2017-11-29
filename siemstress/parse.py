c48f8167e4a4d4124059affde26f2258abb52651
dogoncouch <dpersonsdev@gmail.com>
2017-11-28 21:33:17 -0500

    Clean up comments

--- siemstress/parse.py
+++ siemstress/parse.py
@@ -166,16 +166,6 @@ class LiveParser:
                         if not entry['tzone']:
                             entry['tzone'] = self.tzone
                 
-                    #tstamp = entry['tstamp'].split('.')
-                    ## To Do: remove intdatestamp logic, f_date_stamp column
-                    #intdatestamp = \
-                    #        ymdstamp + tstamp[0]
-                    #
-                    #if len(tstamp) == 1:
-                    #    datestamp = intdatestamp + '.000000'
-                    #else:
-                    #    datestamp = '.'.join(intdatestamp,
-                    #            tstamp[1].ljust(6, '0'))
                     datestamp = ymdstamp + entry['tstamp']
 
                     if not '.' in datestamp:
